0e090efecf1698a54b5e61e14823019cda2f776b
developerhasan99 <developerhasan99@gmail.com>
2023-08-28 22:54:28 +0600

    Fixed header staggering issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useChat } from "./contexts/chatContext";
 import MessageElement from "./Components/MessageElement";
 import Header from "./Components/Header/Header";
 
+import illustration from "./assets/start-your-journey.svg";
+
 export default function App() {
 	const { chat } = useChat();
 
@@ -28,7 +30,7 @@ export default function App() {
 					</div>
 					<div className="col-md-6 d-md-none">
 						<div className="sticky">
-							<img src="src/assets/start-journey.svg" alt="Registration" />
+							<img src={illustration} alt="Registration" />
 							<h1>Your Journey Starts Here</h1>
 							<p style={{ textAlign: "center" }}>
 								&copy; {new Date().getFullYear()} Mehedi Hasan - All rights
